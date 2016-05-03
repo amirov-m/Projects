c8fba6cdb0b0956fabb712fe400ac9b24c0b872f
Mikhail Amirov <amirov.mikhail@gmail.com>
2016-05-03 14:50:20 +0500

    ConsoleApplication commited

---
+++ C#/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Counter
+    {
+        public Counter(int maxNumber)
+        {
+            this.MaxNumber = maxNumber;
+        }
+
+        public int MaxNumber { get; private set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter(10);
+            for (int i = 0; i < counter.MaxNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
