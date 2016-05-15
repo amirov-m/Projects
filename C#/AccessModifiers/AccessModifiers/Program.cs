50937d4c36be2c0bdcb74968df4fccf071297376
nexoroshiy <amirov.mikhail@gmail.com>
2016-05-15 19:07:53 +0500

    AccessModifiers project added

---
+++ C#/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnotherProject;
+
+namespace AccessModifiers
+{
+    class ClassB : ClassA
+    {
+        public ClassB(int d) : base(5)
+        {
+//            protectedField = 10;
+        }
+
+        internal int GetProtectedField()
+        {
+            return protectedField;
+        }
+    }
+
+    class ClassD : ClassC
+    {
+        public ClassD()
+        {
+            protectedInternaField = 13;
+        }
+
+        public int GetField()
+        {
+            return protectedInternaField;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new ClassA(9);
+            var b = new ClassB(10);
+
+            Console.WriteLine(b.GetProtectedField());
+
+            var c = new ClassC();
+            var d = new ClassD();
+            
+            Console.WriteLine(d.GetProtectedInternalField());
+        }
+    }
+}
---
+++ C#/AccessModifiers/AnotherProject/ClassA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherProject
+{
+    public class ClassA
+    {
+        public int field;
+        protected int protectedField;
+
+
+        public ClassA(int field)
+        {
+            this.field = field;
+        }
+    }
+
+    public class ClassC
+    {
+        protected internal int protectedInternaField;
+
+        public int GetProtectedInternalField()
+        {
+            return protectedInternaField;
+        }
+    }
+}
